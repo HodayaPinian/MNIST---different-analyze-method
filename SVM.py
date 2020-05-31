dae1104c4ae20bb7b02d06082a420c342807acff
HodayaPinian <61969606+HodayaPinian@users.noreply.github.com>
2020-05-31 14:14:49 +0300

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -10,6 +10,17 @@ load_data_test = pd.read_csv('mnist_test.csv')
 x_test = load_data_test.iloc[:,1:].values / 255.0
 y_test = load_data_test.iloc[:,0].values
 
+#warning - because this big data, it takes lot of time for calculate. you can choose specific numbers to check the algorithm on.
+#example to choose specific numbers
+"""
+idx = (y_train ==  1) | (y_train == 3) | (y_train == 8) | (y_train == 4)
+x_train = x_train[idx]
+y_train = y_train[idx]
+
+idx2 = (y_test ==  1) | (y_test == 3) | (y_test == 8) | (y_test == 4)
+x_test = x_test[idx2]
+y_test = y_test[idx2]
+"""
 #SVM classifer
 from sklearn.svm import SVC
 classifier = SVC(C=5, gamma=0.05)      
@@ -18,9 +29,12 @@ classifier.fit(x_train, y_train)
 #cross validation accuracy
 from sklearn.model_selection import cross_val_score
 accuracies = cross_val_score(estimator = classifier, X = x_train, y = y_train, cv = 10)
+best_accuracies = accuracies.mean()
+std_accuracies = accuracies.std()
 
 #the best parameters are:
 #for self check, you can use gridSearch to see what parameters are preferred- 
+
 """
 from sklearn.model_selection import GridSearchCV
 parameters = [{'C': [13, 4, 5, 7, 10, 100], 'gamma': [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]},
