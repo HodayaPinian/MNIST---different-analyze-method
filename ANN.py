6f0ed6258db7c5cf839c830e4052be3c2b5e3b36
HodayaPinian <61969606+HodayaPinian@users.noreply.github.com>
2020-06-01 12:12:50 +0300

    Update ANN.py

--- ANN.py
+++ ANN.py
@@ -1,3 +1,4 @@
+# 0.986
 from keras.models import Sequential
 from keras.layers import Dense
 
@@ -6,25 +7,42 @@ import tensorflow as tf
 import numpy as np
 mnist = tf.keras.datasets.mnist 
 input = 28*28
+# Dense needs int for input
 (x_train, y_train), (x_test, y_test) = mnist.load_data() 
 x_train = x_train.reshape([len(x_train),input]) 
-x_train = np.asarray(x_train).astype(np.float32) / 255.0
 x_test = x_test.reshape([len(x_test),input]) 
-x_test = np.asarray(x_test).astype(np.float32) / 255.0
+y_train = np.asarray(y_train).reshape(len(y_train),1)
+y_test = np.asarray(y_test).reshape(len(y_test),1)
 
+#for output layer, we need to encode the labels to 0/1
+from sklearn.preprocessing import OneHotEncoder
+onehotencoder = OneHotEncoder(categories='auto')
+y_train = onehotencoder.fit_transform(y_train).toarray()
+y_test= onehotencoder.fit_transform(y_test).toarray()
+
+#ANN network
 classifier = Sequential()
-classifier.add(Dense(output_dim = input/7, init = 'uniform', activation = 'relu', input_dim = input))
-classifier.add(Dense(output_dim = input/4, init = 'uniform', activation = 'relu'))
-classifier.add(Dense(output_dim = 10, init = 'uniform', activation = 'sigmoid'))
+classifier.add(Dense(112, activation="relu", input_dim=784, kernel_initializer="uniform"))
+classifier.add(Dense(28, activation = 'relu', kernel_initializer = 'uniform'))
+classifier.add(Dense( 10, activation = 'sigmoid',  kernel_initializer = 'uniform'))
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-classifier.fit(x_train, y_train, batch_size = 10, nb_epoch = 100)
-y_pred = classifier.predict(x_test)
-y_pred = (y_pred > 0.5)
+classifier.fit(x_train, y_train, batch_size = 10, epoch = 100)
 
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
+loss, accuracy  = classifier.evaluate(x_test, y_test, verbose=False)
 
+# for visulaize the accuracy trohw epochs : 
+"""
+import matplotlib.pyplot as plt
+history =classifier.fit(x_train, y_train, validation_split = 0.1, batch_size = 100, nb_epoch = 10)
+print(history.history.keys()) #names
+plt.plot(history.history(['accuracy']))
+plt.plot(history.history['val_accuracy'])
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['training', 'validation'], loc='best')
+plt.show()
+
+"""
 
 
 
