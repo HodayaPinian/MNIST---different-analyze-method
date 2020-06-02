678f448e03abb6a4119623289c05a5e2d3a20fc0
HodayaPinian <61969606+HodayaPinian@users.noreply.github.com>
2020-06-02 12:09:14 +0300

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -32,20 +32,3 @@ accuracies = cross_val_score(estimator = classifier, X = x_train, y = y_train, c
 best_accuracies = accuracies.mean()
 std_accuracies = accuracies.std()
 
-#the best parameters are:
-#for self check, you can use gridSearch to see what parameters are preferred- 
-
-"""
-from sklearn.model_selection import GridSearchCV
-parameters = [{'C': [13, 4, 5, 7, 10, 100], 'gamma': [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]},
-              {'C': [13, 4, 5, 7, 10, 100], 'kernel': ['rbf'], 'gamma': [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]},
-              {'C': [13, 4, 5, 7, 10, 100], 'kernel': ['poly'], 'gamma': [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]}]
-grid_search = GridSearchCV(estimator = classifier,
-                           param_grid = parameters,
-                           scoring = 'accuracy',
-                           cv = 10,
-                           n_jobs = -1)
-grid_search = grid_search.fit(x_train, y_train)
-best_accuracy = grid_search.best_score_
-best_parameters = grid_search.best_params_
-"""
